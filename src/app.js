30a8133bff06755ba4f12ae13cc3d4a1739b4a69
Debanga Sarma <deb737@proton.me>
2024-05-11 20:40:20 +0530

    Add generatePlayerData() in utils.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
-const { welcomeMessage } = require("./utils");
-
-console.clear();
+const Arena = require("./components/arena");
+const { generatePlayerData, welcomeMessage } = require("./utils");
 
 welcomeMessage();
+
+let players = null;
+let arena = null;
+
+players = generatePlayerData("data.json");
+
+try {
+	/* check whether players[] have enough number of players. */
+	arena = new Arena(players);
+} catch (e) {
+	console.error(e.message);
+	process.exit(1);
+}
+
+let winner = -1;
+
+/* can simulate any fight here.
+   open data.json to view the data of all players. */
+winner = arena.fight(0, 1);
+
+if (winner != -1) {
+	console.log(`Player ${winner + 1} has won!`);
+}
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,29 @@
+const Player = require("./components/player");
+const fs = require("fs");
+
 const welcomeMessage = () => {
 	console.clear();
 	console.log("Welcome to Magical Arena");
 	console.log("------------------------");
 }
 
+const generatePlayerData = (path) => {
+	const rawData = fs.readFileSync(path);
+	const playerData = JSON.parse(rawData);
+
+	let players = new Array();
+	for (let i = 0; i < playerData.length; i++) {
+		let player = new Player(
+			playerData[i].health,
+			playerData[i].strength,
+			playerData[i].attack
+		)
+		players.push(player);
+	}
+	return players;
+}
+
 module.exports = {
-    welcomeMessage
+	welcomeMessage,
+	generatePlayerData
 }
