eaf26e5bfed76965a20eea7854d9e62bce0b5e8a
Debanga Sarma <deb737@proton.me>
2024-05-11 20:40:19 +0530

    Add welcomeMessage() in utils.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-console.log("Hello world!");
+const { welcomeMessage } = require("./utils");
+
+console.clear();
+
+welcomeMessage();
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+const welcomeMessage = () => {
+	console.clear();
+	console.log("Welcome to Magical Arena");
+	console.log("------------------------");
+}
+
+module.exports = {
+    welcomeMessage
+}
