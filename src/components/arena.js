6eb1c5562ef62f16f2783b207acce24d2ac1465a
Debanga Sarma <deb737@proton.me>
2024-05-11 20:40:20 +0530

    Add error handling and validateFight() in Arena class

--- src/components/arena.js
+++ src/components/arena.js
@@ -5,9 +5,34 @@ class Arena {
 		/* An Arena object contains a players[] array, consisting of Player objects
 		   which is initialized in this constructor. */
 		this.#players = players;
+        if (this.#players.length < 2) {
+			throw new Error("Not enough players.");
+		}
 	}
 
+    validateFight(i, j) {
+		if (i === j) {
+			throw new Error("Both players are same.");
+		}
+
+		this.#players[i].validateStats();
+		this.#players[j].validateStats();
+
+		return -1
+	}
+
+
 	fight(i, j) {
+        /* returns the index of the winning player.
+		   -1 if validateFight() fails. */
+        try {
+			this.validateFight(i, j);
+			console.log("Valid match!");
+		} catch (e) {
+			console.error(e.message);
+			return -1;
+		}
+
 		/* player with less health attacks first. */
 		let chance = this.#players[i].getHealth() < this.#players[j].getHealth();
 		while (this.#players[i].getHealth() > 0 && this.#players[j].getHealth() > 0) {
